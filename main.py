ad23620f1b28dc0fb16a61105465856c7e65b3b0
Daniel24125 <danielmadalena@msn.com>
2024-04-05 13:04:40 +0100

    basic routing implementation

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_restful import Api
+from routes.index import Home
+from routes.test.index import Test
+import inspect
+
+app = Flask(__name__)
+api = Api(app)
+
+routes = [
+    Home,
+    Test
+]
+
+
+def route_registration(): 
+    for r in routes: 
+        route_path = inspect.getfile(r)
+        route_path = inspect.getfile(r).replace("\\", "/")
+        routes_index = route_path.find("routes")+6
+        index_location = route_path.find("index.py")
+        route = route_path[routes_index:index_location]
+        api.add_resource(r, route)
+
+
+if __name__ == '__main__':
+    route_registration()
+    app.run(debug=True)
