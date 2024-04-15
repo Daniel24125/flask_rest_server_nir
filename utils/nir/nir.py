23a11953b4179cbe2419cdd6aeefc2bbafea13fc
Daniel24125 <danielmadalena@msn.com>
2024-04-15 14:50:53 +0100

    trying to read dll

--- utils/nir/nir.py
+++ utils/nir/nir.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     try: 
         print("Loading the dll file")
         dll = WinDLL("./utils/nir/dll/HSSUSB2A.dll")
-        connection = dll.StartMeasure
-        print(dll._name)
-    except Exception as X:
-        print("ERROR: ", X)
+        connection = dll.USB2_setGain
+        print(connection)
+    except AttributeError as X:
+        print(X)
 
 
