d56b23636b105234b33aae6165d456f681cc94cc
Daniel24125 <danielmadalena@msn.com>
2024-04-16 16:21:42 +0100

    experimenting with loading dll files

--- utils/nir/nir.py
+++ utils/nir/nir.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     
     try: 
         print("Loading the dll file")
-        dll = WinDLL("./utils/nir/dll/HSSUSB2A.dll")
-        connection = dll.USB2_setGain
+        dll = WinDLL("./utils/nir/dll/HSSUSB2A_Int.dll")
+        connection = dll.TestUSBFunc()
         print(connection)
     except AttributeError as X:
         print(X)
