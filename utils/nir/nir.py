8dee48da538c8d507547e796616f55168684868f
Daniel24125 <danielmadalena@msn.com>
2024-04-22 15:08:58 +0100

    trying CLR dll

--- utils/nir/nir.py
+++ utils/nir/nir.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     
     try: 
         print("Loading the dll file")
-        dll = WinDLL("./utils/nir/dll/Dll1.dll")
-        connection = dll.InitSpec()
+        dll = WinDLL("./utils/nir/dll/HSSUSB2A_DLL_Interface.dll")
+        connection = dll.GetMeasurement()
         print(connection)
     except AttributeError as X:
         print(X)
